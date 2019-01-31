9a8ead1e870321631d916373affb4164410d916b
Craig Marais <craigm@meerkatio.net>
2019-01-31 13:38:52 +0100

    Fixed broken code

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var app = express();
 
 var path = require('path');
 
-export function my_test(num1, num2) {
-	return num1 + num2;
-}
-
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname + '/index.html'))
 });
